74e67958b9227ffa9664c5c4d454fb1406868074
SteelBoshido99 <55700855+SteelBoshido99@users.noreply.github.com>
2022-02-07 18:02:11 +0000

    New Project
    New project idea Lumigun:
    
    Movement and mouse look implemented

---
+++ PistolTempo/Assets/Scripts/Test3/Player Controller/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public Rigidbody pb;
+
+    private Vector3 moveIn;
+    private Vector3 moveVel;
+
+    private Camera topCamera;
+
+    void Start()
+    {
+        pb = GetComponent<Rigidbody>();
+        topCamera = FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveIn = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveVel = moveIn * speed;
+
+        //Look at mouse
+        Ray cameraRay = topCamera.ScreenPointToRay(Input.mousePosition);
+        Plane ground = new Plane(Vector3.up, Vector3.zero);
+        
+        // how far from the camera to the gorund
+        float rayLength;
+        if(ground.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 lookPoint = cameraRay.GetPoint(rayLength);
+            //Testing
+            //Debug.DrawLine(cameraRay.origin, lookPoint, Color.red);
+
+            transform.LookAt(new Vector3(lookPoint.x, transform.position.y, lookPoint.z)) ;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        pb.velocity = moveVel;
+    }
+
+
+}
