587da8fbf9e8d11e690dba459bcba2f254f9169b
SteelBoshido99 <55700855+SteelBoshido99@users.noreply.github.com>
2022-03-16 19:58:59 +0000

    Playing with light
    Messing around with the light settings and added an outline package

--- PistolTempo/Assets/Scripts/Test3/Player Controller/Firing.cs
+++ PistolTempo/Assets/Scripts/Test3/Player Controller/Firing.cs
@@ -22,12 +22,14 @@ public class Firing : MonoBehaviour
 
     private void Update()
     {
+        //logic to allow the player to switch between bullets
         if (bulletNum == 2 || Input.GetButtonDown("Change"))
         {           
             chosenBullet = bullet;
             bulletNum = 1;
         }
 
+        //logic to allow the player to switch between bullets
         if (bulletNum == 1 || Input.GetButtonDown("Change"))
         {         
             chosenBullet = bullet2;
@@ -35,8 +37,10 @@ public class Firing : MonoBehaviour
         }
 
 
+
         if (Input.GetButtonDown("Shoot1"))
         {
+            //Creates an instance of pointed prefab
             GameObject ball = Instantiate(chosenBullet, transform.position, transform.rotation);
 
             ball.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(bullVelocity, 0, 0));
