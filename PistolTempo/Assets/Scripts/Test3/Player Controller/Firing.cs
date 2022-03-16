33f5b473b98efe55cc66522a0879240c35085bab
SteelBoshido99 <55700855+SteelBoshido99@users.noreply.github.com>
2022-03-16 12:38:24 +0000

    Bullet change
    Adding a mechanic where the player can change what bullets they can shoot with a push of a button

--- PistolTempo/Assets/Scripts/Test3/Player Controller/Firing.cs
+++ PistolTempo/Assets/Scripts/Test3/Player Controller/Firing.cs
@@ -5,14 +5,39 @@ using UnityEngine;
 public class Firing : MonoBehaviour
 {
     [SerializeField] private GameObject bullet;
+    [SerializeField] private GameObject bullet2;
+
+    private int bulletNum;
+
+    private GameObject chosenBullet;
+
     [SerializeField] private float bullVelocity;
+
+    private void Start()
+    {
+        bulletNum = 1;
+        chosenBullet = bullet;
+    }
 
 
     private void Update()
     {
+        if (bulletNum == 2 || Input.GetButtonDown("Change"))
+        {           
+            chosenBullet = bullet;
+            bulletNum = 1;
+        }
+
+        if (bulletNum == 1 || Input.GetButtonDown("Change"))
+        {         
+            chosenBullet = bullet2;
+            bulletNum = 2;
+        }
+
+
         if (Input.GetButtonDown("Shoot1"))
         {
-            GameObject ball = Instantiate(bullet, transform.position, transform.rotation);
+            GameObject ball = Instantiate(chosenBullet, transform.position, transform.rotation);
 
             ball.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(bullVelocity, 0, 0));
         }
