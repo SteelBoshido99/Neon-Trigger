b45360b838b490e15a86205b3e20c9a71db6aeca
SteelBoshido99 <55700855+SteelBoshido99@users.noreply.github.com>
2022-04-14 20:24:55 +0100

    Bullet Ricochet
    Implemented bullet ricochet, I also fixed a bug where the bullet sometimes phases through the walls

--- PistolTempo/Assets/Scripts/Test3/BulletMechanics/BulletRicochet.cs
+++ PistolTempo/Assets/Scripts/Test3/BulletMechanics/BulletRicochet.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class BulletRicochet : MonoBehaviour
 {
     [SerializeField] private float bulletLife = 3.0f;
-
+    public LayerMask collision;
     // Update is called once per frame
     void Update()
     {
-        //Ray ray = new Ray(transform.position, transform.forward);
-        //RaycastHit impact;
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit impact;
 
-        //if (Physics.Raycast(ray, out impact, Time.deltaTime)){
-
-        //}
+        if (Physics.Raycast(ray, out impact, Time.deltaTime + 0.05f, collision))
+        {
+            Vector3 ricochetDir = Vector3.Reflect(ray.direction, impact.normal);
+            float rotation = 90 - Mathf.Atan2(ricochetDir.z, ricochetDir.x) * Mathf.Rad2Deg;
+            transform.eulerAngles = new Vector3(0, rotation, 0);
+        }
 
         GameObject bullet = this.gameObject;
         Destroy(bullet, bulletLife);
