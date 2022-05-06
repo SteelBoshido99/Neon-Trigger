e4921728979da7e67195854ad2290b44c44f71af
SteelBoshido99 <55700855+SteelBoshido99@users.noreply.github.com>
2022-05-06 23:35:20 +0100

    Enemy AI
    Starting to implement enemy AI, using a system where a random point is created. Blue enemy moves

--- PistolTempo/Assets/Scripts/NeonTrigger/Enemy script/EnemyAI.cs
+++ PistolTempo/Assets/Scripts/NeonTrigger/Enemy script/EnemyAI.cs
@@ -5,16 +5,25 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    [SerializeField] private NavMeshAgent enemyAgent;
     // Update is called once per frame
     void Update()
     {
-        
+        enemyAgent.SetDestination(randomNavigation(4f));
     }
+
+    //This gets a random point in the navmesh
+    public Vector3 randomNavigation(float radius)
+    {
+        Vector3  randomPoint = Random.insideUnitSphere * radius;
+        randomPoint += transform.position;
+        NavMeshHit hit;
+        Vector3 finalPos = Vector3.zero;
+        if(NavMesh.SamplePosition(randomPoint, out hit, radius, 1))
+        {
+            finalPos = hit.position;
+        }
+        return finalPos;
+    }
+
 }
